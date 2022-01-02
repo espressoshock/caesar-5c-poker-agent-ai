5b110296cbbb59b44234efabaf6c8a71b086e0eb
espressoshock <vincebshock@gmail.com>
2022-01-02 05:34:20 +0100

    ✨feat(Memorizer): round winner selected impl. added

--- round.py
+++ round.py
@@ -3,20 +3,56 @@
 #######################################################################
 
 # Imports
+from player import Player
+import enum
+
+##################
+#  Victory type  #
+##################
+class WinType(enum.Enum):
+    UNDISPUTED = (0,)
+    DISPUTED = 1
 
 
 class Round:
-    ############
-    #  Player  #
-    ############
-    class Player:
-        def __init__(self, chips: int):
-            self.chips = chips
-
     #################
     #  Constructor  #
     #################
-    def __init__(self, ante: int, opponents: list):
-        self._number = 0
-        self._ante = ante
-        self._opponents = {o.name: self.Player(o.chips) for o in opponents}
+    # opponents: {name: chips}
+    # number: server round number
+    def __init__(self, number: int, ante: int, opponents: list):
+        self.number = number
+        self.ante = ante
+
+        # =============
+        # = Opponents =
+        # =============
+        # for simplicity
+        # access actions
+        # directly
+        self.opponents = {name: Player(chips) for name, chips in opponents}
+
+        # =================
+        # = Round Results =
+        # =================
+        # available after
+        # showdown
+        self.results = None
+
+    # =======================
+    # = Update player chips =
+    # =======================
+    def update_chips(self, player: str, chips: int) -> None:
+        self.opponents[player].update_chips(chips)
+
+    # ===============
+    # = Update ante =
+    # ===============
+    def update_ante(self, chips: int) -> None:
+        self.ante = chips
+
+    # ========================
+    # = Winner been selected =
+    # ========================
+    def over(self, wtype: WinType, player: str, amount: int) -> None:
+        self.results = {"w_type": wtype, "p_name": player, "w_amount": amount}
