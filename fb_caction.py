51a410f8903628f651ab454978e3cdbe31327b21
espressoshock <vincebshock@gmail.com>
2022-01-02 22:25:05 +0100

    ✨feat(FB-CAction): get_pairs added

--- fb_caction.py
+++ fb_caction.py
@@ -86,12 +86,30 @@ class FB_cAction:
                 return c
         return None  # no pairs
 
+    @staticmethod
+    def get_pairs(hand: list) -> str:
+        values = dict(zip("23456789TJQKA", range(2, 15)))
+
+        def sortkey(x):
+            value, suit = x
+            return values[value], suit
+
+        srt = sorted(hand, key=sortkey, reverse=True)
+        seen = []
+        pairs = []
+        for c in srt:
+            if c not in seen:
+                seen.append(c)
+            else:
+                pairs.append(c)
+                seen.remove(c)
+        return pairs  # no pairs
+
     #######################################################################
     #                         Losing probability                          #
     #######################################################################
     @staticmethod
     def _having_pair(hand: list) -> float:
-        print("given: ", hand)
         # ==========
         # = Params =
         # ==========
@@ -125,7 +143,44 @@ class FB_cAction:
             + FB_cAction.HandRanks.ROYAL_FLUSH.value[1]
         )
 
+    @staticmethod
+    def _having_2pair(hand: list) -> float:
+        print("given: ", hand)
+        # ==========
+        # = Params =
+        # ==========
+        max_card = FB_cAction.get_max(hand)
+        pair = FB_cAction.get_pair(hand)
+        all_comb = math.comb(52, 5)
+
+        # =========
+        # = Cases =
+        # =========
+        def _diffs(x):
+            diffs = dict(zip("AKQJT98765432", range(0, 13)))
+            value, suit = x
+            return diffs[value]
+
+        ncr42 = math.comb(4, 2)
+        ncr123 = math.comb(12, 3)
+        better_pair = (_diffs(pair) * ncr42 * ncr123 * (4 ** 3)) / all_comb
+        higher = (_diffs(max_card) * 4) / all_comb
+
+        return (
+            higher
+            + better_pair
+            + FB_cAction.HandRanks.THREE_OF_A_KIND.value[1]
+            + FB_cAction.HandRanks.STRAIGHT.value[1]
+            + FB_cAction.HandRanks.FLUSH.value[1]
+            + FB_cAction.HandRanks.FULL_HOUSE.value[1]
+            + FB_cAction.HandRanks.FOUR_OF_A_KIND.value[1]
+            + FB_cAction.HandRanks.STRAIGHT_FLUSH.value[1]
+            + FB_cAction.HandRanks.ROYAL_FLUSH.value[1]
+        )
+
 
 # print(FB_cAction._having_pair(["3c", "Tc", "3c", "Aj", "Th"]))
-print(FB_cAction._having_pair(["2c", "5s", "5s", "8j", "Th"]))
+# print(FB_cAction._having_pair(["2c", "5s", "5s", "8j", "Th"]))
+# print(FB_cAction._having_2pair(["2c", "5s", "5s", "8j", "Th"]))
 # print(FB_cAction.get_pair(["3c", "Tc", "3c", "Aj", "Th"]))
+# print(FB_cAction.get_pairs(["5c", "Tc", "3c", "Tc", "Th"]))
